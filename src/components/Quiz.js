cf1244ce79a5ff7fd5a8926887f9954e961ca5e8
Andy Lawson <alawson89@gmail.com>
2021-05-14 10:01:41 -0700

    successfully refactor quiz to implement hooks

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -16,6 +16,7 @@ const QuestionCard = styled.div`
 `
 
 const Quiz = () => {
+  const user = firebase.auth().currentUser;
   const [quizEdition, setQuizEdition] = useState('Basic');
   const [quizStart, setQuizStart] = useState(false);
   const [quizChoice, setQuizChoice] = useState(false);
@@ -32,8 +33,132 @@ const Quiz = () => {
   const [a3, setA3] = useState('');
   const [a4, setA4] = useState('');
 
+  const startQuiz = () => {
+    if (quizEdition === 'Basic') {
+      basicQuizStart();
+    }
+    setQuizStart(false);
+    setQuizChoice(false);
+    setInitialQuizState(false);
+  };
+  const basicQuizStart = () => {
+    const toggleBtn = document.getElementById('startBtn');
+    toggleBtn.style.display = 'none';
+    setStartBasic(true);
+    setQuestionNum( currentQuestion => currentQuestion + 1 );
+    loadNewQuestion();
+  };
+  const loadNewQuestion = () => {
+    if (quizEdition === 'Basic') {
+      setQuestion(BasicQuestions[questionNum].question);
+      setA1(BasicQuestions[questionNum].a1);
+      setA2(BasicQuestions[questionNum].a2);
+      setA3(BasicQuestions[questionNum].a3);
+      setA4(BasicQuestions[questionNum].a4);
+      setCorrect(BasicQuestions[questionNum].correct);
+      setTotalQuestions(BasicQuestions.length);
+    };
+  };
+  const checkAnswer = e => {
+    setQuestionNum( currentQuestion => currentQuestion + 1 );
+    jsonCheck(e);
+  };
+  const jsonCheck = e => {
+    let id = e.target.getAttribute('data_id');
+    if (id === correct) {
+      setPlayerScore( currentPlayerScore => currentPlayerScore + 1 );
+    }
+    console.log('questionNum', questionNum);
+    if (questionNum < totalQuestions) {
+      loadNewQuestion();
+    } else {
+      gameOver();
+    }
+  };
+  const gameOver = () => {
+    setQuizReset(true);
+  }
 
+  if (user) {
+    return (
+      <QuestionCard>
+        <button id='startBtn' onClick={basicQuizStart}>Start Quiz</button>
+        {startBasic ? (
+          <>
+            <div>
+              {!quizReset ? (
+                <>
+                  <h3 className='quiz-question-number'>
+                    Q. {questionNum}
+                  </h3>
+                  <h2 className='quiz-question'>{question}</h2>
+                  <button
+                    className='btn btn-primary game-buttons'
+                    onClick={checkAnswer}
+                    data_id='1'
+                  >
+                    {a1}
+                  </button>
+                  <button
+                    className='btn btn-primary game-buttons'
+                    onClick={checkAnswer}
+                    data_id='2'
+                  >
+                    {a2}
+                  </button>
+                  <button
+                    className='btn btn-primary game-buttons'
+                    onClick={checkAnswer}
+                    data_id='3'
+                  >
+                    {a3}
+                  </button>
+                  <button
+                    className='btn btn-primary game-buttons'
+                    onClick={checkAnswer}
+                    data_id='4'
+                  >
+                    {a4}
+                  </button>
+                </>
+              ) : (
+                ''
+              )}
+            </div>
+            <div>
+              {quizReset ? (
+                <>
+                  <div className='quiz-player-score'>
+                    <h1>
+                      Final Score - {playerScore} /{' '}
+                      {totalQuestions}
+                    </h1>
+                    <hr></hr>
+                    <h3>
+                      {(
+                        (playerScore / totalQuestions) * 100
+                      ).toFixed(2)}
+                      %
+                    </h3>
+                  </div>
+                </>
+              ) : (
+                ''
+              )}
+            </div>
+          </>
+        ) : (
+          ''
+        )}
+      </QuestionCard>
+    )} else {
+    return (
+      <h3 style={{color: "red", textAlign: 'center'}}>You must sign in to view content</h3>
+    );
+  }
 };
+
+export default Quiz;
 
 
 // class Quiz extends Component {
@@ -61,143 +186,6 @@ const Quiz = () => {
 //       quizStart: true
 //     });
 //   };
-//   startQuiz = () => {
-//     if (this.state.quizEdition === 'Basic') {
-//       this.basicQuizStart();
-//     }
-//     this.setState({
-//       quizStart: false,
-//       quizChoice: false,
-//       initialQuizState: false
-//     });
-//   };
-//   basicQuizStart = () => {
-//     const toggleBtn = document.getElementById('startBtn');
-//     toggleBtn.style.display = 'none';
-//     this.setState({
-//       startBasic: true,
-//       questionNum: this.state.questionNum + 1
-//     });
-//     this.loadNewQuestion();
-//   };
-//   loadNewQuestion = () => {
-//     if (this.state.quizEdition === 'Basic') {
-//       this.setState({
-//         question: BasicQuestions[this.state.questionNum].question,
-//         a1: BasicQuestions[this.state.questionNum].a1,
-//         a2: BasicQuestions[this.state.questionNum].a2,
-//         a3: BasicQuestions[this.state.questionNum].a3,
-//         a4: BasicQuestions[this.state.questionNum].a4,
-//         correct: BasicQuestions[this.state.questionNum].correct,
-//         totalQuestions: BasicQuestions.length
-//       });
-//     };
-//   };
-//   checkAnswer = e => {
-//     this.setState({
-//       questionNum: this.state.questionNum + 1
-//     });
-//     this.jsonCheck(e);
-//   };
-//   jsonCheck = e => {
-//     let id = e.target.getAttribute('data_id');
-//     if (id === this.state.correct) {
-//       this.setState({
-//         playerScore: this.state.playerScore + 1
-//       });
-//     }
-//     console.log('questionNum', this.state.questionNum);
-//     if (this.state.questionNum < this.state.totalQuestions) {
-//       this.loadNewQuestion();
-//     } else {
-//       this.gameOver();
-//     }
-//   };
-//   gameOver = () => {
-//     this.setState({
-//       quizReset: true,
-//     });
-//   }
 // render() {
-//   const user = firebase.auth().currentUser;
-//   if (user) {
-//     return (
-//       <QuestionCard>
-//         <button id='startBtn' onClick={this.basicQuizStart}>Start Quiz</button>
-//         {this.state.startBasic ? (
-//           <>
-//             <div>
-//               {!this.state.quizReset ? (
-//                 <>
-//                   <h3 className='quiz-question-number'>
-//                     Q. {this.state.questionNum}
-//                   </h3>
-//                   <h2 className='quiz-question'>{this.state.question}</h2>
-//                   <button
-//                     className='btn btn-primary game-buttons'
-//                     onClick={this.checkAnswer}
-//                     data_id='1'
-//                   >
-//                     {this.state.a1}
-//                   </button>
-//                   <button
-//                     className='btn btn-primary game-buttons'
-//                     onClick={this.checkAnswer}
-//                     data_id='2'
-//                   >
-//                     {this.state.a2}
-//                   </button>
-//                   <button
-//                     className='btn btn-primary game-buttons'
-//                     onClick={this.checkAnswer}
-//                     data_id='3'
-//                   >
-//                     {this.state.a3}
-//                   </button>
-//                   <button
-//                     className='btn btn-primary game-buttons'
-//                     onClick={this.checkAnswer}
-//                     data_id='4'
-//                   >
-//                     {this.state.a4}
-//                   </button>
-//                 </>
-//               ) : (
-//                 ''
-//               )}
-//             </div>
-//             <div>
-//               {this.state.quizReset ? (
-//                 <>
-//                   <div className='quiz-player-score'>
-//                     <h1>
-//                       Final Score - {this.state.playerScore} /{' '}
-//                       {this.state.totalQuestions}
-//                     </h1>
-//                     <hr></hr>
-//                     <h3>
-//                       {(
-//                         (this.state.playerScore / this.state.totalQuestions) * 100
-//                       ).toFixed(2)}
-//                       %
-//                     </h3>
-//                   </div>
-//                 </>
-//               ) : (
-//                 ''
-//               )}
-//             </div>
-//           </>
-//         ) : (
-//           ''
-//         )}
-//       </QuestionCard>
-//     )}  else {
-//       return (
-//         <h3 style={{color: "red", textAlign: 'center'}}>You must sign in to view content</h3>
-//       );
-//     }
-//   }
-// }
+//   
 
-export default Quiz;
